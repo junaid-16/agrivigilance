68d9bf89136a8d3134acf645885243c07be38d68
Karan Owalekar <karan-owalekar@users.noreply.github.com>
2021-04-23 13:41:17 +0530

    Hid the api keys.

---
+++ web/config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDrseb_mj-VSsZmhoGY35ZjCCdT0dc7ItM",
+    authDomain: "agrivigilance-25edc.firebaseapp.com",
+    projectId: "agrivigilance-25edc",
+    storageBucket: "agrivigilance-25edc.appspot.com",
+    messagingSenderId: "933459027389",
+    appId: "1:933459027389:web:9035489f7d018f23425ed4",
+    measurementId: "G-D7DERVPW83"
+  };
